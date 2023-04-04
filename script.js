9f7f7f8db6e31d3667d93e1d863c41ee63af4bf5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-04 15:24:24 +0200

    add get element by id

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const name = prompt (`Qual'è il tuo nome?`);
 const surname = prompt(`Qual'è il tuo cognome?`);
-const color = prompt(`QUAL'è il tuo colore preferito?`);
+const color = prompt(`Qual'è il tuo colore preferito?`);
+
+// document.getElementById (`password`).innerHTML = 
