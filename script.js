85f1eb3acdd2a6877d2b4037c3099e844f6faeda
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-04 16:01:39 +0200

    Add favNumber constant

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const properName = prompt (`Qual'è il tuo nome?`);
 const properSurname = prompt(`Qual'è il tuo cognome?`);
 const favColor = prompt(`Qual'è il tuo colore preferito?`);
+const favNumber = prompt (`Qual'è il tuo numero preferito?`);
 
-document.getElementById (`password`).innerHTML = properName + properSurname + favColor 
+document.getElementById (`password`).innerHTML = properName + properSurname + favColor + favNumber
