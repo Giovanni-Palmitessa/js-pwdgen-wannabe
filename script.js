d290dcfad4a31592e62dbf3b645ea5bb55e313ee
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-04 17:02:31 +0200

    add $

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ const properSurname = prompt(`Qual'è il tuo cognome?`);
 const favColor = prompt(`Qual'è il tuo colore preferito?`);
 const favNumber = prompt (`Qual'è il tuo numero preferito?`);
 
-document.getElementById (`password`).innerHTML = properName + properSurname + favColor + favNumber
+const newPassword = properName + properSurname + favColor + favNumber;
+
+document.getElementById (`password`).innerHTML = `La tua password è: ${newPassword}`;
+
+
+
+const numero1 = parseInt(prompt(`Dammi un numero`));
+const numero2 = parseInt(prompt(`Dammi un altro numero`));
+
+document.getElementById (`fav-number`).innerHTML = numero1 + numero2 + 1
