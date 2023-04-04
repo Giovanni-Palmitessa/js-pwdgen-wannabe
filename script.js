24f0dd1610f6acffba124488175e03affba69008
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-04 15:15:09 +0200

    add surname constant

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-const name = prompt ('Come ti chiami?')
+const name = prompt (`Qual'è il tuo nome?`);
+const surname = prompt(`Qual'è il tuo cognome?`);
+// const color = prompt(`QUAL'è il tuo colore preferito?`);
