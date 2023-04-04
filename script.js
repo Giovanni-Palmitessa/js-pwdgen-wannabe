0f8b1b7d35dfaba3ef9076435e0819753270ef8d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-04 15:15:36 +0200

    add color constant

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const name = prompt (`Qual'è il tuo nome?`);
 const surname = prompt(`Qual'è il tuo cognome?`);
-// const color = prompt(`QUAL'è il tuo colore preferito?`);
+const color = prompt(`QUAL'è il tuo colore preferito?`);
